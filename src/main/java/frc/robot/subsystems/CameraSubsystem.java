8452667f84262e5f9cfcf4b3b3f0a142e5293a9d
Geneva-Robovikes <robovikes@outlook.com>
2023-01-20 18:33:59 -0600

    Added some cam methods

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -5,11 +5,10 @@
 package frc.robot.subsystems;
 
 import java.util.List;
-
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -33,6 +32,10 @@ public class CameraSubsystem extends SubsystemBase {
     return result.hasTargets();
   }
 
+  /**
+   * Sets the camera's pipeline to the index's corresponding pipeline.
+   * @param index The index of the pipeline. 0 = Tag, 1 = Cone, 2 = Cube, 3 = Tape
+   */
   public void setPipeline(int index) {
     camera.setPipelineIndex(index);
   }
