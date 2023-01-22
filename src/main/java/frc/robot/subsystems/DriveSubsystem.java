5f52adf221a6438375db02f623f07b8e2f86667b
Geneva-Robovikes <robovikes@outlook.com>
2023-01-21 22:38:43 -0600

    Added javadoc comments

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -46,6 +46,11 @@ public class DriveSubsystem extends SubsystemBase {
     new Pose2d(0, 0, new Rotation2d()));
   Pose2d currenPose2d;
 
+  /**
+   * Creates a DriveSubsystem with the provided motor indexes and cameras.
+   * @param motorIndexes A list of 8 indexes to drive the robot with.
+   * @param cameras A list of cameras for use in odometry estimation.
+   */
   public DriveSubsystem(int[] motorIndexes, CameraSubsystem[] cameras) {
     this.cameras = cameras;
     frontLeftModule = new SwerveModule(motorIndexes[0], motorIndexes[1]);
@@ -72,11 +77,21 @@ public class DriveSubsystem extends SubsystemBase {
     }
   }
 
+  /**
+   * Sets the swerve modules according to the privided chassis speeds.
+   * @param xVelocity Velocity in the x direction, away from driver station is +x
+   * @param yVelocity Velocity in the y direction, left of driver station is +y
+   * @param angularVelocity Angular velocity, counter clockwise is +θ
+   */
   public void setModuleStatesFromSpeeds(double xVelocity, double yVelocity, double angularVelocity) {
     ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(xVelocity, yVelocity, angularVelocity, new Rotation2d(gyro.getGyroAngleZ()));
     setModuleStates(kinematics.toSwerveModuleStates(speeds));
   }
 
+  /**
+   * Sets the odometry to the new pose.
+   * @param pose The pose to set to.
+   */
   public void resetOdometry(Pose2d pose) {
     odometry.resetPosition(new Rotation2d(gyro.getGyroAngleZ()), 
       new SwerveModulePosition[] {
@@ -88,18 +103,35 @@ public class DriveSubsystem extends SubsystemBase {
     );
   }
 
+  /**
+   * Gets the current pose stored in the odometry.
+   * @return The robot's pose in meters.
+   */
   public Pose2d getPose() {
     return odometry.getEstimatedPosition();
   }
 
+  /**
+   * Gets the current rotation of the robot.
+   * @return The robot's rotation as Rotation2d
+   */
   public Rotation2d getRotation2d() {
     return new Rotation2d(gyro.getGyroAngleZ());
   }
   
-  public void addCameraToOdometry(Optional<EstimatedRobotPose> estimatedPose, double timestamp) {
+  /**
+   * Adds the estimated pose from a camera to the odometry calculations.
+   * @param estimatedPose The estimated pose from the camera.
+   * @param timestamp The timestamp the data was taken from.
+   */
+  void addCameraToOdometry(Optional<EstimatedRobotPose> estimatedPose, double timestamp) {
     odometry.addVisionMeasurement(currenPose2d, timestamp);
   }
 
+  /**
+   * Sets the module states to the provided SwerveModuleStates.
+   * @param moduleStates The states to set to.
+   */
   void setModuleStates(SwerveModuleState[] moduleStates) {
     frontLeftModule.setSesiredState(moduleStates[0]);
     frontRightModule.setSesiredState(moduleStates[1]);
