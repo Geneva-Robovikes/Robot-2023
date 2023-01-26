93948ec8edcff269156bd5e588f0edba209f23dc
Robovikes <robovikes@geneva304.org>
2023-01-26 15:14:21 -0600

    Added balance command

--- src/main/java/frc/robot/commands/BalanceCommand.java
+++ src/main/java/frc/robot/commands/BalanceCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
 
 /** An example command that uses an example subsystem. */
 public class BalanceCommand extends CommandBase {
