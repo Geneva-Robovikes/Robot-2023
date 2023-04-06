aa247276dc757ef5e4e27d813ff6ccfe8c49db7f
Robovikes <robovikes@geneva304.org>
2023-04-06 16:42:50 -0500

    Commented the claw subsystem

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -17,6 +17,9 @@ public class ClawSubsystem extends SubsystemBase {
     private final DigitalInput pivotLimitSwitchTop;
     private final DigitalInput pivotLimitSwitchBottom;
 
+    /**
+     * Creates the Claw Subsystem. This controls claw rotation and intaking/outtaking.
+     */
     public ClawSubsystem(){
         pivotMotor = new WPI_TalonFX(9);
         clawMotor = new CANSparkMax(8, MotorType.kBrushless);
@@ -28,35 +31,66 @@ public class ClawSubsystem extends SubsystemBase {
         pivotMotor.setNeutralMode(NeutralMode.Brake);
     }
 
-    public void setClawMotor(double value){
-        clawMotor.set(value);
+    /**
+     * Sets the intake motor to the specified speed.
+     * @param speed The motor speed. Must be between -1.0 and 1.0
+     */
+    public void setClawMotor(double speed){
+        clawMotor.set(speed);
     }
 
+    /**
+     * Returns the applied current to the intake motor.
+     * @return the applied current in amps.
+     */
     public double getClawMotorCurrent() {
         SmartDashboard.putNumber("Neo Current", clawMotor.getOutputCurrent());
         return clawMotor.getOutputCurrent();
     }
 
+    /**
+     * Sets the pivot motor to the specified speed.
+     * @param speed The speed to set the motor. Must be between -1.0 and 1.0
+     */
     public void setPivotMotor(double speed){
         pivotMotor.set(speed);
     }
 
+    /**
+     * Returns the state of the pivot's top limit switch.
+     * @return The top switch state.
+     */
     public boolean getPivotTopState() {
         return !pivotLimitSwitchTop.get();
     }
 
+    /**
+     * Returns the state of the pivot's bottom limit switch.
+     * @return The top switch state.
+     */
     public boolean getPivotBottomState() {
         return !pivotLimitSwitchBottom.get();
     }
 
+    /**
+     * Returns the distance the pivot motor has travelled.
+     * @return The distance the motor has travelled in encoder units.
+     */
     public double getPivotDistance() {
         return pivotMotor.getSelectedSensorPosition();
     }
     
+    /**
+     * Reset the position of the pivot motor encoder.
+     */
     public void resetPivotEncoder() {
         pivotMotor.setSelectedSensorPosition(0);
     }
 
+    /**
+     * Returns the sngle the claw has travelled.
+     * @return The angle of the claw in radians.
+     */
     public double getClawAngle() {
         return pivotMotor.getSelectedSensorPosition() / Constants.falconEncoderResolution / Constants.clawAngleGearRatio * (2 * Math.PI);
     }
